76a15da198079bfe24c69b600c3ab577e2525d15
Yoon <yun.hyeok2001@gmail.com>
2025-01-21 22:37:36 +0900

    fullPage 오류 및 AOS 추가

--- Portfolio.js
+++ Portfolio.js
@@ -1,17 +1,4 @@
-//페이지 이동 함수
-
-fullpage_api.moveTo("section2"); // 두 번째 섹션으로 이동
-fullpage_api.moveSectionDown(); // 다음 섹션으로 이동
-fullpage_api.moveSectionUp(); // 이전 섹션으로 이동
-
-window.onload = function () {
-  if (window.location.hash) {
-    history.replaceState(null, null, window.location.pathname);
-  }
-};
-
-new fullpage("#fullpage", {
-  autoScrolling: true,
-  navigation: true,
-  scrollingSpeed: 700, // 속도를 느리게 설정
+AOS.init({
+  duration: 1000,
+  once: false, // 애니메이션 반복
 });
