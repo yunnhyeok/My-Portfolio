43834a436a0b41b60d54c45ecab0f68cb3f72a15
Yoon <yun.hyeok2001@gmail.com>
2025-01-22 17:36:11 +0900

    page2 add

--- Portfolio.js
+++ Portfolio.js
@@ -1,4 +1,8 @@
 AOS.init({
-  duration: 1000,
+  duration: 800,
   once: false, // 애니메이션 반복
+  mirror: true, // 스크롤 올릴 때도 재실행
+  once: false, // 여러 번 재실행
+  mirror: true, // 스크롤 올릴 때도 실행
+  offset: 200, // 스크롤 위치 (200px 이후 애니메이션 시작)
 });
