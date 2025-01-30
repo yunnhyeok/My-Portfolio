7ab26f79d5ea502d672963d8614b5cad5c8c8620
Yoon <yun.hyeok2001@gmail.com>
2025-01-30 21:31:26 +0900

    project 부분 수정

--- Portfolio.js
+++ Portfolio.js
@@ -46,3 +46,12 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+function openModal(title) {
+  document.getElementById("modal-title").innerText = title;
+  document.getElementById("modal").style.display = "flex";
+}
+
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
+}
